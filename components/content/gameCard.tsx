62cdc95c9ae09929e0cd3c7484a7b69103156011
Maryanne Kaffer <kaffermaryanne@gmail.com>
2025-02-09 23:59:14 -0300

    optimizations

--- components/content/gameCard.tsx
+++ components/content/gameCard.tsx
@@ -1,6 +1,10 @@
 import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
 import { Image } from "@heroui/image";
 
+interface GameCardProps {
+  games: any[];
+}
+
 function Score({ rating }: { rating: number }) {
   if (rating > 4) {
     return <span className="text-green-500">{rating}</span>;
@@ -14,26 +18,28 @@ function Score({ rating }: { rating: number }) {
 export default function GameCard({ games }: { games: any[] }) {
   return (
     <div className="grid grid-cols-4 gap-5 mt-5">
-      {games.map((game) => (
-        <Card key={game.id} className="max-w-[400px]">
-          <CardHeader className="flex gap-3">
-            <Image
-              src={game.background_image}
-              alt={game.name}
-              className="w-full h-56 object-cover"
-            />
-          </CardHeader>
-          <CardBody>
-            <h3 className="text-xl font-bold">{game.name}</h3>
-            <p className="text-sm mt-2">Released: {game.released}</p>
-            <p className="text-sm mt-2">Rating: <Score rating={game.rating} /></p>
-          </CardBody>
-          <CardFooter>
-            <p className="text-sm">
-              Genres: {game.genres.map((genre: any) => genre.name).join(", ")}
-            </p>
-          </CardFooter>
-        </Card>
+      {games.map((game, index) => (
+        <div key={`${game.id}-${index}`}> 
+          <Card className="max-w-[400px]">
+            <CardHeader className="flex gap-3">
+              <Image
+                src={game.background_image}
+                alt={game.name}
+                className="w-full h-56 object-cover"
+              />
+            </CardHeader>
+            <CardBody>
+              <h3 className="text-xl font-bold">{game.name}</h3>
+              <p className="text-sm mt-2">Released: {game.released}</p>
+              <p className="text-sm mt-2">Rating: <Score rating={game.rating} /></p>
+            </CardBody>
+            <CardFooter>
+              <p className="text-sm">
+                Genres: {game.genres.map((genre: any) => genre.name).join(", ")}
+              </p>
+            </CardFooter>
+          </Card>
+        </div>
       ))}
     </div>
   );
--- src/app/api/proxy.ts
+++ src/app/api/proxy.ts
@@ -1,8 +1,14 @@
-export default async function fetchGames() {
+export default async function fetchGames(page: number) {
   const apiKey = process.env.NEXT_PUBLIC_RAWG_API_KEY;
-  const response = await fetch(`https://api.rawg.io/api/games?key=${apiKey}`);
+  const pageSize = 12; 
+  
+  const response = await fetch(
+    `https://api.rawg.io/api/games?key=${apiKey}&page=${page}&page_size=${pageSize}`
+  );
+
   if (!response.ok) {
     throw new Error("Failed to fetch games");
   }
+
   return response.json();
 }
